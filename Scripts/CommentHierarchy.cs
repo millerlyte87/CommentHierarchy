d0031382b29c359a47bcd412ac3e2979deb22316
Matt Miller <matt>
2020-08-22 08:35:20 -0700

    CommentHierarchy: Added TODO

--- Scripts/CommentHierarchy.cs
+++ Scripts/CommentHierarchy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,17 +14,20 @@ namespace MeFirstGames.Tools
         private static readonly Vector2 MagicOffset = new Vector2(18, 0f);
 
         // Editor only font
-        private static readonly Color EditorOnlyFontColorActive = new Color(0.03f, 0.86f, 0f);
-        private static readonly Color EditorOnlyFontColorInactive = new Color(0.03f, 0.53f, 0f);
-        private static readonly Color EditorOnlyPrefabFontColorActive =  new Color(0f, 0.86f, 0.39f);
-        private static readonly Color EditorOnlyPrefabFontColorInactive =  new Color(0f, 0.62f, 0.31f);
+        private static readonly Color EditorOnlyCommentFontColorActive = new Color(0.03f, 0.86f, 0f);
+        private static readonly Color EditorOnlyCommentFontColorInactive = new Color(0.03f, 0.53f, 0f);
+        private static readonly Color EditorOnlyCommentPrefabFontColorActive =  new Color(0f, 0.86f, 0.39f);
+        private static readonly Color EditorOnlyCommentPrefabFontColorInactive =  new Color(0f, 0.62f, 0.31f);
+        private static readonly Color EditorOnlyTodoFontColorActive = new Color(0.92f, 0.45f, 0.93f);
+        private static readonly Color EditorOnlyTodoFontColorInactive = new Color(0.74f, 0.38f, 0.75f);
+        private static readonly Color EditorOnlyTodoPrefabFontColorActive = new Color(0.68f, 0.37f, 0.69f);
+        private static readonly Color EditorOnlyTodoPrefabFontColorInactive = new Color(0.56f, 0.29f, 0.57f);
         
         private const string EDITOR_ONLY = "EditorOnly";
 
         static CommentHierarchy()
         {
             EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemOnGUI;
-            EditorApplication.update += OnUpdate;
         }
 
         private static void OnHierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
@@ -34,58 +38,125 @@ namespace MeFirstGames.Tools
                 var gameObject = obj as GameObject;
                 if (gameObject != null)
                 {
-                    // Highlight Editor only
-                    if (gameObject.tag.Equals(EDITOR_ONLY))
+                    var name = gameObject.name;
+
+                    if (HasPrefix(name, "TODO")
+                        || HasPrefix(name, "//TODO"))
                     {
-                        Color textColor;
-                        if (PrefabUtility.IsPartOfAnyPrefab(gameObject))
-                        {
-                            textColor = gameObject.activeInHierarchy
-                                ? EditorOnlyPrefabFontColorActive
-                                : EditorOnlyPrefabFontColorInactive;
-                        }
-                        else
-                        {
-                            textColor = gameObject.activeInHierarchy
-                                ? EditorOnlyFontColorActive
-                                : EditorOnlyFontColorInactive;
-                        }
-                        
-                        Rect offsetRect = new Rect(selectionRect.position + MagicOffset, selectionRect.size);
-                        EditorGUI.LabelField(offsetRect, gameObject.name, new GUIStyle()
-                            {
-                                normal = new GUIStyleState() {textColor = textColor},
-                                fontStyle = FontStyle.Normal,
-                            }
-                        );
+                        DrawLabelField(gameObject,
+                            selectionRect,
+                            EditorOnlyTodoFontColorActive,
+                            EditorOnlyTodoFontColorInactive,
+                            EditorOnlyTodoPrefabFontColorActive,
+                            EditorOnlyTodoPrefabFontColorInactive);
+                    }
+                    else if (HasPrefix(name, "//"))
+                    {
+                        DrawLabelField(gameObject,
+                            selectionRect,
+                            EditorOnlyCommentFontColorActive,
+                            EditorOnlyCommentFontColorInactive,
+                            EditorOnlyCommentPrefabFontColorActive,
+                            EditorOnlyCommentPrefabFontColorInactive);
                     }
                 }
             }
         }
+        
+        #region Helper Methods
 
-        private static void OnUpdate()
+        /// <summary>
+        /// Draws a label with a color scheme
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="selectionRect"></param>
+        /// <param name="activeColor"></param>
+        /// <param name="inactiveColor"></param>
+        /// <param name="activePrefabColor"></param>
+        /// <param name="inactivePrefabColor"></param>
+        private static void DrawLabelField(GameObject gameObject,
+            Rect selectionRect,
+            Color activeColor,
+            Color inactiveColor,
+            Color activePrefabColor,
+            Color inactivePrefabColor)
         {
-            if (Application.isPlaying)
-                return;
-
-            foreach (var gameObject in Selection.gameObjects)
+            // Try to set the editor 
+            SetToEditorOnly(gameObject);
+                
+            if (gameObject.tag.Equals(EDITOR_ONLY))
             {
-                SetToEditorOnly(gameObject);
+                Color textColor;
+                if (PrefabUtility.IsPartOfAnyPrefab(gameObject))
+                {
+                    textColor = gameObject.activeInHierarchy
+                        ? activePrefabColor
+                        : inactivePrefabColor;
+                }
+                else
+                {
+                    textColor = gameObject.activeInHierarchy
+                        ? activeColor
+                        : inactiveColor;
+                }
+
+                Rect offsetRect = new Rect(selectionRect.position + MagicOffset, selectionRect.size);
+                EditorGUI.LabelField(offsetRect, gameObject.name, new GUIStyle()
+                    {
+                        normal = new GUIStyleState() {textColor = textColor},
+                        fontStyle = FontStyle.Normal,
+                    }
+                );
             }
         }
-
+        
+        /// <summary>
+        /// Sets a gameobject to the EditorOnly tag
+        /// </summary>
+        /// <param name="gameObject"></param>
         private static void SetToEditorOnly(GameObject gameObject)
         {
-            // Force comment to be editor only
-            if (!gameObject.tag.Equals(EDITOR_ONLY)
-                && gameObject.name.Length > 1
-                && gameObject.name[0] == '/'
-                && gameObject.name[1] == '/')
+            if (!gameObject.tag.Equals(EDITOR_ONLY))
             {
                 gameObject.tag = EDITOR_ONLY;
                 EditorUtility.SetDirty(gameObject);
             }
         }
+
+        /// <summary>
+        /// Checks to see if a prefix is in a string.
+        /// Case insensitive and ignores whitespace.
+        /// Does not allocate memory.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        private static bool HasPrefix(string text, string prefix)
+        {
+            var i = 0;
+            var j = 0;
+            while (i < prefix.Length)
+            {
+                if (i >= text.Length || j > text.Length)
+                    break;
+                
+                var character = text[j];
+                if (!char.IsWhiteSpace(character))
+                {
+                    var prefixCharacter = prefix[i];
+                    if (char.ToLower(character) != char.ToLower(prefixCharacter))
+                        break;
+
+                    ++i; // Increment Prefix
+                }
+
+                ++j; // Increment text
+            }
+
+            return i == prefix.Length;
+        }
+        
+        #endregion
     }
     
     #endif
